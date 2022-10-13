0eb1e9ef4f7e5309b93d7c62330a116c20a8aebb
elena tarasova <nafanichka.ev@gmail.com>
2022-10-13 19:38:07 -0400

    Exersize2

---
+++ Desktop/HW9/Exersize2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Write a number M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Write a number N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int SumArray (int m, int n)
+{
+    if(m == n)
+    {
+        return m;
+    }
+    return n + SumArray(m, n-1);   
+}
+Console.WriteLine(SumArray(m, n));
