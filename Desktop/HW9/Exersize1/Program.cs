7707b45cf22b4819b95fbc95ac2b2bf4ae88b920
elena tarasova <nafanichka.ev@gmail.com>
2022-10-13 19:36:43 -0400

    Exersize1

---
+++ Desktop/HW9/Exersize1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Write a number N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+string Recurs (int n, int minValue)
+{
+    if(minValue == n)
+    {
+        return n.ToString();
+    }
+    return (n + " " + Recurs(n-1, minValue));
+}
+Console.WriteLine(Recurs(n, 1));
